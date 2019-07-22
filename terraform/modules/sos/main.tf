49dc4eb4d54e563562b54173565140ae4452b1cb
Trevor Facer <trevorf@ifit.com>
2019-07-21 22:44:05 -0600

    Add IAM user, key, and policy
    This adds IAM for the sos application

--- terraform/modules/sos/main.tf
+++ terraform/modules/sos/main.tf
@@ -23,3 +23,43 @@ resource "aws_s3_bucket" "bucket" {
     Name = var.resource_prefix
   }
 }
+
+resource "aws_iam_user" "user" {
+  name = "sos"
+  path = "/"
+
+  tags = {
+    Name = var.resource_prefix
+  }
+}
+
+resource "aws_iam_access_key" "key" {
+  user = "${aws_iam_user.user.name}"
+}
+
+resource "aws_iam_user_policy" "policy" {
+  name = "sos-user"
+  user = "${aws_iam_user.user.name}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:ListAllMyBuckets"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:s3:::save-our-summer-images/*"
+    }
+  ]
+}
+EOF
+}
