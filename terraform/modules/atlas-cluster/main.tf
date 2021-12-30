5bd879f14f11def8ea20a1058ac81d90415dfb2d
Trevor Facer <trevorf@ifit.com>
2021-12-30 12:50:58 -0700

    Add variables for user

--- terraform/modules/atlas-cluster/main.tf
+++ terraform/modules/atlas-cluster/main.tf
@@ -17,6 +17,10 @@ variable "cluster_version" {
 # database
 variable "database_name" {}
 
+# user
+variable "user_name" {}
+variable "user_password" {}
+
 resource "mongodbatlas_project" "project" {
   name   = var.project_name
   org_id = var.org_id
